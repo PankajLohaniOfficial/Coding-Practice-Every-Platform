fa488bfa4871a22b809edcbaab88804166de8c7a
Pankaj Lohani <pankajlohani2023@gmail.com>
2025-09-01 13:20:33 +0530

    commit

---
+++ Leetcode 485 - Maximum No of Consecutive Ones/code.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int maxim = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 1)
+            {
+                count++;
+                maxim = max(maxim, count);
+            }
+
+            else
+            {
+                count = 0;
+            }
+        }
+        return maxim;
+    }
+};
