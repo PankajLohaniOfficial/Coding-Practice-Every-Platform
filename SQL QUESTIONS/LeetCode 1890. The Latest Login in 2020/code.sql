0537c818c2e8e0b88b36caa2d0c20756239732a4
Pankaj Lohani <pankajlohani2023@gmail.com>
2025-09-28 16:45:50 +0530

    code

--- SQL QUESTIONS/LeetCode 1890. The Latest Login in 2020/code.sql
+++ SQL QUESTIONS/LeetCode 1890. The Latest Login in 2020/code.sql
@@ -1,4 +1,5 @@
-SELECT user_id, MAX(time_stamp) AS last_stamp
+SELECT user_id, MAX(time_stamp) AS last_stamp 
+-- here max is used to find the latest one
 FROM Logins
 WHERE YEAR(time_stamp) = 2020
 GROUP BY user_id;
